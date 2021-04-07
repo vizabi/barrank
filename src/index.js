86790a0a3d08c8207fdaa4b2b6d9f1899fbacca5
Angie <konzeptmeister@gmail.com>
2021-04-07 16:48:35 +0200

    v3.3.0: Remove setting language to marker data config

--- src/index.js
+++ src/index.js
@@ -83,11 +83,6 @@ export default class BarRankChart extends BaseComponent {
       layout: new LayoutService(config.layout)
     };
 
-    //register locale service in the marker model
-    config.model.config.markers.bar.data.locale = observable({
-      get id() { return config.services.locale.id; }
-    });
-
     super(config);
   }
 }
