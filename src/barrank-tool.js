502245ab1b90006a9e8a43512b9e61a8bdb66b5d
Angie Hjort <konzeptmeister@gmail.com>
2025-11-22 20:35:19 +0100

    Move configs to the TOOL and add defensive features

--- src/barrank-tool.js
+++ src/barrank-tool.js
@@ -22,15 +22,14 @@ export default class BarRank extends BaseComponent {
   
   constructor(config){
     
-    const markerName = config.options?.markerNames?.bar || "bar";
-    const fullMarker = config.model.markers[markerName];
-    config.Vizabi.utils.applyDefaults(fullMarker.config, BarRank.DEFAULT_CORE(markerName));
+    const fullMarker = config.model.markers?.bar;
+    const fullMarkerLegend = config.model.markers?.legend;
+    config.Vizabi.utils.applyDefaults(fullMarker?.config || {}, BarRank.DEFAULT_MODEL.bar);   
+    config.Vizabi.utils.applyDefaults(fullMarkerLegend?.config || {}, BarRank.DEFAULT_MODEL.legend);  
 
     const frameType = config.Vizabi.stores.encodings.modelTypes.frame;
     const { marker, splashMarker } = frameType.splashMarker(fullMarker);
-
-    config.model.markers[markerName] = marker;
-
+    
     config.name = "barrank";
 
     config.subcomponents = [{
@@ -116,41 +115,122 @@ export default class BarRank extends BaseComponent {
 
 
 BarRank.DEFAULT_UI = {
-  chart: {
+  "locale": { "id": "en", "shortNumberFormat": true },
+  "layout": { "projector": false },
+
+  "buttons": {
+    "buttons": ["markercontrols", "colors", "moreoptions", "presentation", "sidebarcollapse", "fullscreen"]
+  },
+  "dialogs": {
+    "dialogs": {
+      "popup": ["timedisplay", "colors", "markercontrols", "moreoptions"],
+      "sidebar": ["timedisplay", "colors", "markercontrols"],
+      "moreoptions": ["opacity", "speed", "colors", "repeat", "technical", "presentation", "about"]
+    },
+    "markercontrols": {
+      "disableSlice": true,
+      "disableAddRemoveGroups": true,
+      "primaryDim": null,
+      "drilldown": null,
+      "shortcutForSwitch": false,
+      "shortcutForSwitch_allow": null
+    }
+  },
+  "marker-contextmenu": {
+    "primaryDim": null,
+    "drilldown": null,
+  },
+  "time-slider": {
+    "show_value": false
+  },
+  "chart": {
+    "lilFrameDisplayAlwaysHidden": true,
+    "showForecast": false,
+    "showForecastOverlay": true,
+    "pauseBeforeForecast": true,
+    "endBeforeForecast": null, //value like "2022", auto-resolved to current time minus one frame step
+    "opacityHighlight": 1.0,
+    "opacitySelect": 1.0,
+    "opacityHighlightDim": 0.3,
+    "opacitySelectDim": 0.5,
+    "opacityRegular": 1.0
+  },
+  "data-warning": {
+    "enable": false,
+    "margin": {
+      "LARGE": { "bottom": 90 },
+      "MEDIUM": { "bottom": 70 },
+      "SMALL": { "bottom": 50 }
+    }
+  },
+  "tree-menu": {
+    "showDataSources": false,
+    "folderStrategyByDataset": {}
   }
 };
 
-BarRank.DEFAULT_CORE = () => ({
-  encoding: {
-    selected: {
-      modelType: "selection"
-    },
-    highlighted: {
-      modelType: "selection"
-    },
-    x: {
-      scale: {
-        allowedTypes: ["linear", "log", "genericLog", "pow"]
+BarRank.DEFAULT_MODEL = {
+  "bar": {
+    "requiredEncodings": ["x"],
+    "encoding": {
+      "show": { "modelType": "selection" },
+      "selected": { "modelType": "selection" },
+      "highlighted": { "modelType": "selection" },
+      "x": {
+        "data": { },
+        "scale": {
+          "allowedTypes": ["linear", "log", "genericLog", "pow"]
+        }
+      },
+      "color": {
+        "data": { "constant": "_default" },
+        "scale": {
+          "modelType": "color"
+        }
+      },
+      "label": { "data": { "modelType": "entityPropertyDataConfig" } },
+      "frame": { "modelType": "frame", "speed": 200, "splash": true },
+      "repeat": {
+        "modelType": "repeat",
+        "allowEnc": ["x"]
       }
-    },
-    color: {
-      scale: {
-        modelType: "color"
-      }
-    },
-    label: {
-      data: {
-        modelType: "entityPropertyDataConfig"
-      }
-    },
-    frame: {
-      modelType: "frame",
-    },
-    repeat: {
-      modelType: "repeat",
-      allowEnc: ["x"]
     }
-  }
-});
+  },
+  "legend": {
+    "data": {
+      "ref": {
+        "transform": "entityConceptSkipFilter",
+        "path": "markers.bar.encoding.color"
+      }
+    },
+    "encoding": {
+      "color": {
+        "data": {
+          "concept": { "ref": "markers.bar.encoding.color.data.concept" },
+          "constant": { "ref": "markers.bar.encoding.color.data.constant" }
+        },
+        "scale": {
+          "modelType": "color",
+          "palette": { "ref": "markers.bar.encoding.color.scale.palette" },
+          "domain": null,
+          "range": null,
+          "type": null,
+          "zoomed": null,
+          "zeroBaseline": false,
+          "clamp": false,
+          "allowedTypes": null
+        }
+        //"scale": { "ref": "markers.bar.encoding.color.scale" }
+      },
+      "name": { "data": { } },
+      "order": {
+        "modelType": "order",
+        "direction": "asc",
+        "data": { }
+      },
+      "map": { "data": { } }
+    }
+  },
+};
 
 BarRank.versionInfo = { version: __VERSION, build: __BUILD, package: __PACKAGE_JSON_FIELDS, sharedComponents: versionInfo};
